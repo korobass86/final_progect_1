65d0f6c67d2cd67f49a825111e67b5dfd25e23c1
Dmitry Korobov <korobov.da86@gmail.com>
2024-03-01 20:07:06 +0300

    Add FileService, PathService, FileAccessException

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hi, user! I'm caesar descryptor. Choose you task:\n" +
                 "5 - encrypt file with key\n" +
-                "2 - decrypt file with key\n" +
+                "4 - decrypt file with key\n" +
                 "3 - brute force decrypt file\n" +
-                "4 - statistic analize descriptor");
+                "2 - statistic analize descriptor");
     }
 }
